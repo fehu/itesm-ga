ea06b62d84b2b307c63fa1150ac72461d0f988ac
fehu <lord.fehu@gmail.com>
2016-04-08 18:23:30 -0500

    working on `ChromosomeExample`;

--- src/Parcial2/Examples/ChromosomeExample.hs
+++ src/Parcial2/Examples/ChromosomeExample.hs
@@ -13,6 +13,7 @@
 
 module Main where
 
+import GeneticAlgorithm
 import Parcial2.Labyrinth
 import Parcial2.Tikz
 import Parcial2.Tikz.DrawCrossover
@@ -37,51 +38,17 @@ main = do -- print $ uncurry (tikzCromosome 1 ["draw"]) chExViolet
 --
 --          print $ tikzRoutes [] 1 $ attrs `zip` routes'
 
+          let ga = GA (labyrinth2D labyrinthExample) undefined undefined
+              (_, dd) = crossover' ga (fst chExViolet) (fst chExOrange)
+
           print $ tikzCrossover 1 chExViolet
                                 2 chExOrange
                                 undefined
                                 undefined
 
 
-subseq xs l r = take (r - l) . drop l
 
 
-foo l ch1 ch2 = let rs1 = splitRoutes l ch1
-                    rs2 = splitRoutes l ch2
-
-                    set1 = Set.fromList ch1
-                    set2 = Set.fromList ch2
-                    cs = Set.toList $ Set.intersection set1 set2
-
-                    isrs = do x <- cs
-                              y <- cs
-                              let valid = any ((&&) <$> (x `elem`) <*> (y `elem`))
-                                  valid1 = valid rs1
-                                  valid2 = valid rs2
-
-                                  valid' ch = let Just ix = x `elemIndex` ch
-                                                  Just iy = y `elemIndex` ch
-
-                                                  (left, right, rev) = if ix < iy then (ix,iy,False)
-                                                                                  else (iy,ix,True)
-                                         in (subseq ch left right, rev)
-
-                                  valid1' = if valid1 then Just $ valid' ch1 else Nothing
-                                  valid2' = if valid2 then Just $ valid' ch2 else Nothing
-
-                              if x /= y && (valid1 || valid2)
-                                then return ((x,y), (valid1', valid2'))
-                                else []
-
-
---                    rs1 = splitRoutes l ch1
---                    rs2 = splitRoutes l ch2
-
---                    re1 = [r | r <- rs1, r `notElem` rs2]
---                    re2 = [r | r <- rs2, r `notElem` rs1]
-
---                in (splitRoutes l re1, splitRoutes l re2)
-                in undefined
 -----------------------------------------------------------------------------
 
 --data ChromExample
